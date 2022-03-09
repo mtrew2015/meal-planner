b8c98f99da9284e776603ec53f54602530c710de
Michael Trew <mtrew2015@gmail.com>
2022-03-09 13:26:23 -0500

    deleting

--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const MONGO_DB_CONNECTION_STRING =
-  process.env.MONGO_DB_CONNECTION_STRING || 'mongodb://localhost/nest';
--- src/ingredient/ingredient.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IngredientResolver } from './ingredient.resolver';
-
-@Module({
-  providers: [IngredientResolver],
-})
-export class IngredientModule {}
